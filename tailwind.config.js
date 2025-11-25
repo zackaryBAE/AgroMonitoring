f126bc5940cc0beeb62de1057ed588d91d7ff36e
Zack <111754328+zackaryBAE@users.noreply.github.com>
2025-11-25 13:23:50 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: "#eafaf0",
+          100: "#c8f2d6",
+          200: "#99e8b6",
+          300: "#67dd94",
+          400: "#36d373",
+          500: "#1fba5a",
+          600: "#169045",
+          700: "#0e6731",
+          800: "#07411d",
+          900: "#041f0e"
+        }
+      }
+    }
+  },
+  darkMode: "class",
+  plugins: []
+};
